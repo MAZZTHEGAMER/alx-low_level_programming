ec218ded758b0a9dcecdff796c148e74d0303985
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2022-12-08 15:31:35 +0100

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 {
     int charSize = sizeof(char);
     int intSize = sizeof(int);
-    int longSize = sizeof(long int);
+    int longSize = sizeof(long);
     int doubleSize = sizeof(double);
     int floatSize = sizeof(float);
 
