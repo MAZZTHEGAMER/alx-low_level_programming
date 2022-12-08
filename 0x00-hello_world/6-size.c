cf7e0e5eee7b6bec498e97197bb72a9abc428299
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2022-12-08 15:28:52 +0100

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@
 {
     int charSize = sizeof(char);
     int intSize = sizeof(int);
-    int longIntSize = sizeof(long int);
+    int longSize = sizeof(long int);
     int doubleSize = sizeof(double);
     int floatSize = sizeof(float);
 
     printf("Size of a char: %i byte(s),\n", charSize);
     printf("Size of an int: %i byte(s),\n", intSize);
-    printf("Size of a long int: %i byte(s),\n", longIntSize);
-    printf("Size of a long long int: %i byte(s),\n", longIntSize);
+    printf("Size of a long int: %i byte(s),\n", longSize);
+    printf("Size of a long long int: %i byte(s),\n", doubleSize);
     printf("Size of a float: %i byte(s),\n", floatSize);
     return (0);
 }
