7924ac9ed2971036176375765bd6f66d82e0f1c0
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2022-12-08 15:26:15 +0100

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+    int charSize = sizeof(char);
+    int intSize = sizeof(int);
+    int longIntSize = sizeof(long int);
+    int doubleSize = sizeof(double);
+    int floatSize = sizeof(float);
+
+    printf("Size of a char: %i byte(s),\n", charSize);
+    printf("Size of an int: %i byte(s),\n", intSize);
+    printf("Size of a long int: %i byte(s),\n", longIntSize);
+    printf("Size of a long long int: %i byte(s),\n", longIntSize);
+    printf("Size of a float: %i byte(s),\n", floatSize);
+    return (0);
+}
