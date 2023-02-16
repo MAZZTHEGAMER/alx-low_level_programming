993b80c1fb710ad6a8241ba68e654e505324aa57
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-16 15:17:46 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
- int main(void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-    printf(stderr, 
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+printf(stderr, 
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
