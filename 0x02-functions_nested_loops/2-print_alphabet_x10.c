7eb818b782dbe8c5afd8f2db3b38e71f7540c287
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-22 10:45:12 +0000

    for

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,37 @@
 #include "main.h"
 /**
-*print_alphabet_x10 - print all small letters ten times
-*
+*print_alphabet_x10-printallalphabetx10
 */
 void print_alphabet_x10(void)
 {
-int letters;
-int count;
-count = 0;
-while (count < 10)
+char i, letters;
+
+for (i = '0'; i <= '9'; i++)
 {
-for (letters = 97; letters < 123; letters++)
+for (letters = 'a'; letters <= 'z'; letters++)
 {
 _putchar(letters);
 }
 _putchar('\n');
-count++;
 }
 }
+// #include "main.h"
+// /**
+// *print_alphabet_x10 - print all small letters ten times
+// *
+// */
+// void print_alphabet_x10(void)
+// {
+// int letters;
+// int count;
+// count = 0;
+// while (count < 10)
+// {
+// for (letters = 97; letters < 123; letters++)
+// {
+// _putchar(letters);
+// }
+// _putchar('\n');
+// count++;
+// }
+// }
