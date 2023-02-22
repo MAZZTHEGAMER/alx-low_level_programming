2319bb5ff769bcf397d93227f18c492cff9dc37a
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-22 11:30:15 +0000

    nautral.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int num;
+int sum;
+
+sum = 0;
+for (num = 0; num < 1024; num++)
+{
+if (num % 3 == 0 || num % 5 == 0)
+{
+sum += num;
+}
+}
+printf("%d", sum);
+printf("\n");
+return (0);
+}
