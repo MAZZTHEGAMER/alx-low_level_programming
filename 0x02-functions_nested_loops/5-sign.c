b932ab3ce603c4f7c9ed6f54d229340eaab98b8d
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-22 11:10:13 +0000

    sign it

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_sign-printsthesignsofanumber
+*@n:thenumberofsignstobeprinted
+*Return:1ifnumberisgreaterthanzero
+*0ifnumberiszero
+*-1ifnumberislesthanzero
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+else
+{
+_putchar('-');
+}
+}
