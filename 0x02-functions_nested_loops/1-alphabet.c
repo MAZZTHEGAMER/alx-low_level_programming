ca86062c588232c9a57ccc802fa45c71ccb79495
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-22 09:45:58 +0000

    alphabetical

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ */
+void print_alphabet(void)
+{
+int alphabets;
+for (alphabets = 97; alphabets < 123; alphabets++)
+{
+_putchar(alphabets);
+}
+_putchar('\n');
+}
