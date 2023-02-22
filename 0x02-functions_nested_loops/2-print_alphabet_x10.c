8a0d1b8f4700353719ba7ca001d3841d9b32cb28
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-22 10:42:01 +0000

    rock

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - print all small letters ten times
- *
- */
+*print_alphabet_x10 - print all small letters ten times
+*
+*/
 void print_alphabet_x10(void)
 {
 int letters;
