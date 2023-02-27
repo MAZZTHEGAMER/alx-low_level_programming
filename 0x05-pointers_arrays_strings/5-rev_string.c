b7c07d2aebc6795161f179aedf035b2887d8e5e3
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-27 16:27:03 +0000

    puts

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,22 @@
 */
 void rev_string(char *s)
 {
-int i, j, k, tempVal;
+	int i, j, k, tempVal;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-j = i - 1;
-k = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+	k = 0;
 
-while (k < j)
-{
-tempVal = s[k];
-s[k] = s[j];
-s[j] = tempVal;
-k++;
-j--;
-}
+	while (k < j)
+	{
+		tempVal = s[k];
+		s[k] = s[j];
+		s[j] = tempVal;
+		k++;
+		j--;
+	}
 }
