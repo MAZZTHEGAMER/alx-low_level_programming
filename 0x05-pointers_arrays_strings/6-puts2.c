9c875bb87b2ba6a2ca7734f6c393cdf4b00349d9
Mas'ud Hashim <89136415+MAZZTHEGAMER@users.noreply.github.com>
2023-02-27 16:27:38 +0000

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,19 +10,19 @@
 */
 void puts2(char *str)
 {
-    int i;
-    int c = 0;
+int i;
+int c = 0;
 
-    while (str[c] != '\0')
-    {
-        c++;
-    }
+while (str[c] != '\0')
+{
+c++;
+}
 
-    for (i = 0; i < c; i++)
-    {
-        if (i % 2 != 0)
-            continue;
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+for (i = 0; i < c; i++)
+{
+if (i % 2 != 0)
+continue;
+_putchar(str[i]);
+}
+_putchar('\n');
 }
